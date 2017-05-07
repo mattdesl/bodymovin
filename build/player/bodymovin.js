3212474f303910339bc5cce41913edf9412a7340
mattdesl <dave.des@gmail.com>
2017-05-07 18:46:28 -0400

    add color lerp to canvas solids

--- build/player/bodymovin.js
+++ build/player/bodymovin.js
@@ -9218,8 +9218,23 @@ CVSolidElement.prototype.renderFrame = function(parentMatrix){
     var ctx = this.canvasContext;
     this.globalData.renderer.save();
     this.globalData.renderer.ctxTransform(this.finalTransform.mat.props);
-    this.globalData.renderer.ctxOpacity(this.finalTransform.opacity);
-    ctx.fillStyle=this.data.sc;
+
+    var color = this.data.sc;
+    var opacity = this.finalTransform.opacity;
+    if (this.effects && this.effects.effectElements && this.effects.effectElements[0]) {
+        const efGroup = this.effects.effectElements[0];
+        if (efGroup.effectElements.length >= 3) {
+            var rgb = efGroup.effectElements[2].p.v;
+            color = 'rgb('+bm_floor(rgb[0]*255)+','+bm_floor(rgb[1]*255)+','+bm_floor(rgb[2]*255)+')';
+        }
+        if (efGroup.effectElements.length >= 7) {
+            var alpha = efGroup.effectElements[6].p.v;
+            opacity *= alpha;
+        }
+    }
+
+    this.globalData.renderer.ctxOpacity(opacity);
+    ctx.fillStyle=color;
     ctx.fillRect(0,0,this.data.sw,this.data.sh);
     this.globalData.renderer.restore(this.data.hasMask);
     if(this.firstFrame){
